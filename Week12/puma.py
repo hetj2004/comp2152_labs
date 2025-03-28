588de4c9c77319a719883a16ec7822912003db33
hetj2004 <het28jasani.com>
2025-03-28 08:49:34 -0400

    w12 complete

--- Week12/puma.py
+++ Week12/puma.py
@@ -1,17 +1,10 @@
-from mammal import Mammal
+# Tick class for aggregation
+class Tick:
+    def __init__(self):
+        print("Aggregation: Tick is created")
 
-class Puma(Mammal):
-    def __init__(self, p_age, tick):
-        print("Constructor: Inside the Child class constructor: Adding the Puma parts of a puma")
-        # Call the parent constructor
-        
-
-        # Aggregation: store tick reference
-        
-
-        # Puma-specific fields
-        
-
+    def suck_blood(self):
+        print("Tick is feeding")
+    
     def __del__(self):
-        print("Destructor: The garbage collector is now deleting the Puma part of the object")
-        
+        print("Aggregation: Tick is destroyed.")
